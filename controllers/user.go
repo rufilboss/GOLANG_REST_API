6ac266c51c51bfcf70055b4f88b0095152d6e58a
Ilyas Rufai <rufaiilyas@gmail.com>
2024-03-29 18:55:20 +0100

    update

--- controllers/user.go
+++ controllers/user.go
@@ -1,5 +1,47 @@
 package controllers
 
 import(
-	
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type UserController struct{
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController{
+	return &UserController{s}
+}
+
+
+// Funtion to get the user id and
+func (uc UserController) GetUser (w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+	if err := uc.Session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil {
+	w.WriteHeader(404)
+	return
+	}
+
+	uj, err := json.Marshal(u)
+	if err!= nil{
+		fmt.Println(err)
+	}
+
+w.Header().Set("Content-Type", "application/json")
+w.WriteHeader(http.StatusOK)
+fmt.Fprintf(w, "%s\n", uj)
+}
+
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-import(
+import (
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
-	"net/http"
 )
 
 func main() {
+	r := httprouter.New()
+	uc := controllers.NewController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
 
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
